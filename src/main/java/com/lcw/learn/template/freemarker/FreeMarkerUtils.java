331bee24acfeaed7c3789d9103fe02d4f644be81
leicongwu <917592309@qq.com>
2018-05-04 12:15:54 +0800

    模板引擎、freemarker

---
+++ src/main/java/com/lcw/learn/template/freemarker/FreeMarkerUtils.java
@@ -0,0 +1,56 @@
+package com.lcw.learn.template.freemarker;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+/**
+ * Created by engle on 2018/5/4.
+ */
+public class FreeMarkerUtils {
+    public Template getTemplate(String name){
+        Configuration cfg = new Configuration();
+        Template template = null;
+        try {
+            cfg.setDirectoryForTemplateLoading(new File("D:\\git\\learn\\src\\main\\java\\com\\lcw\\learn\\template\\freemarker\\ftl"));
+            template = cfg.getTemplate(name);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return template;
+    }
+
+    public void print(String name,Map<String,Object> root){
+        Template template = getTemplate(name);
+        try {
+            template.process(root, new PrintWriter(System.out));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fprint(String name,Map<String,Object> root,String outFileName){
+        FileWriter fileWrite = null;
+        try {
+            fileWrite = new FileWriter(outFileName);
+            Template template = getTemplate(name);
+            template.process(root, fileWrite);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            if(fileWrite != null){
+                try {
+                    fileWrite.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/com/lcw/learn/template/freemarker/FreemarkerTest.java
@@ -0,0 +1,19 @@
+package com.lcw.learn.template.freemarker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by engle on 2018/5/4.
+ */
+public class FreemarkerTest {
+
+    public static void main(String[] args) {
+        FreeMarkerUtils freeUtils = new FreeMarkerUtils();
+        Map<String, Object> root = new HashMap<String, Object>();
+        root.put("username", "李四");
+        freeUtils.print("01.ftl", root);
+        freeUtils.fprint("02.ftl", root,"D:\\01.html");
+
+    }
+}
