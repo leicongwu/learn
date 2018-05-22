261f9055d11df8738517f3db72c0cc3300653a74
leicongwu <917592309@qq.com>
2018-05-22 14:44:18 +0800

    jsoup解析完善，图书

--- src/main/java/com/lcw/learn/net/parse/JsoupHtmlParser.java
+++ src/main/java/com/lcw/learn/net/parse/JsoupHtmlParser.java
@@ -5,6 +5,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
 /**
  * Created by leiconwu on 2018/5/18.
  *
@@ -17,13 +21,33 @@ import org.jsoup.select.Elements;
 public class JsoupHtmlParser {
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://nuoha.com/book/168270/00001.html").get();
-        log(doc.title());
-        body > div.book > div.box01 > div.recom > ul > li:nth-child(1)
+        Document doc = Jsoup.connect("https://nuoha.com/book/168270.html").get();
+        Elements newsHeadline = doc.select("body > div.book > div.box01 > div.left > div.chapter > div > ul > li > a");
 
-        Elements newsHeadlines = doc.select("body > div.book > div.box01 > div.left > div.chapter > div.row02");
-        for (Element headline : newsHeadlines) {
-            log(headline.text());
+        //一切都是最好的安排 - 诺哈网
+        String bookName = doc.title();
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("D://",bookName+".txt")));
+        if(newsHeadline!=null && newsHeadline.size() > 0) {
+            int size = newsHeadline.size();
+            for (int i = size-1 ; i >= 0; i--){
+                Element element = newsHeadline.get(i);
+                String title = element.text();
+                bufferedWriter.write(title);
+                bufferedWriter.newLine();
+                String url = element.absUrl("href");
+                Document document = Jsoup.connect(url).get();
+
+                Elements newsHeadlines = document.select("body > div.book > div.box01 > div.left > div.chapter > div.row02");
+                if(newsHeadlines!=null && newsHeadlines.size() > 0){
+                    Element textEle = newsHeadlines.get(0);
+                    String content = textEle.text();
+                    bufferedWriter.write(content);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                }
+            }
+            bufferedWriter.flush();
+            bufferedWriter.close();
         }
     }
 
