d3e3d5988dc2a27126efdb77ec825fa8d5c0e677
leicongwu <917592309@qq.com>
2018-06-12 17:38:16 +0800

    jsoup解析完善，图书

--- src/main/java/com/lcw/learn/net/parse/JsoupHtmlParser.java
+++ src/main/java/com/lcw/learn/net/parse/JsoupHtmlParser.java
@@ -17,11 +17,20 @@ import java.io.FileWriter;
  *         Modification History:
  *         Date         Author      Version     Description
  *         ------------------------------------------------------------------
+ *         仅限个人使用
  */
 public class JsoupHtmlParser {
 
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://nuoha.com/book/35547.html").get();
+        new JsoupHtmlParser().downNuoHaWeb();
+    }
+
+    public static void parserText(){
+
+    }
+
+    public void downNuoHaWeb() throws Exception{
+        Document doc = Jsoup.connect("https://nuoha.com/book/48088.html").get();
         Elements newsHeadline = doc.select("body > div.book > div.box01 > div.left > div.chapter > div > ul > li > a");
 
         //一切都是最好的安排 - 诺哈网
@@ -51,8 +60,36 @@ public class JsoupHtmlParser {
         }
     }
 
-    public static void parserText(){
+    public void downJyliWeb() throws Exception{
+        Document doc = Jsoup.connect("http://book.jiyili.net/0/58/index.html").get();
+        Elements newsHeadline = doc.select("#wp > div > div.novel_volume > div.novel_list > ul > li > a");
+        //#wp > div > div.novel_volume > div:nth-child(1)
+        ////*[@id="wp"]/div/div[3]/div[1]
+        //一切都是最好的安排 - 诺哈网
+        String bookName = doc.title().split("-")[0];
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File("D://",bookName+".txt")));
+        if(newsHeadline!=null && newsHeadline.size() > 0) {
+            int size = newsHeadline.size();
+            for (int i = 0 ; i < size; i++){
+                Element element = newsHeadline.get(i);
+                String title = element.text();
+                bufferedWriter.write(title);
+                bufferedWriter.newLine();
+                String url = element.absUrl("href");
+                Document document = Jsoup.connect(url).get();
 
+                Elements newsHeadlines = document.select("#content > div.novel_content");
+                if(newsHeadlines!=null && newsHeadlines.size() > 0){
+                    Element textEle = newsHeadlines.get(0);
+                    String content = textEle.text();
+                    bufferedWriter.write(content);
+                    bufferedWriter.newLine();
+                    bufferedWriter.flush();
+                }
+            }
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        }
     }
 
     public static void log(String  text){
